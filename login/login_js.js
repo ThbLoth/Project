7785522c53b74938a0948bac1c7775d2c1bf13d6
PL-Cretinon <73227087+P-LouisCre@users.noreply.github.com>
2022-03-19 16:19:57 -0400

    functiuns register and login

--- login/login_js.js
+++ login/login_js.js
@@ -2,21 +2,14 @@ function verification_login(){
     let mail = document.forms["login_page"]["mail_login"].value;
     let password = document.forms["login_page"]["password_login"].value;
 
-    let passwordfor_login = /[a-zA-Z0-9._-]/;
-
     if (mail == "" || password == ""){
         alert("Fill in all the fields");
-        return false;
-    }
-
-    if (!password.match(passwordfor_login)){
-        alert("Wrong Format!");
-        return false;
+        
     }
 
     else{
+        alert(mail + " " + password)
         
-        return false;
     }
     
 
@@ -28,23 +21,22 @@ function verification_register(){
     let name_1 = document.forms["registration_page"]["name"].value;
     let last_name_1 = document.forms["registration_page"]["last_name"].value;
 
-
-    let passwordfor_register = /[a-zA-Z0-9._-]/;
-
     if (mail_1 == "" || password_1 == "" || name_1 == "" || last_name_1 == ""){
         alert("Fill in all the fields");
-        return false;
-    }
-
-    if (!password.match(passwordfor_register)){
-        alert("Wrong Format!");
-        return false;
     }
 
     else{
         
-        return false;
     }
     
 
 }
+
+/*
+var emaillog = document.getElementById("email_log").value;
+var passlog = document.getElementById("password_log").value;
+
+var namereg = document.getElementById("name_reg").value;
+var lstnamereg = document.getElementById("last_name_reg").value;
+var emailreg = document.getElementById("email_reg").value;
+var passreg = document.getElementById("password_reg").value;*/
